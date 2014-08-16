c398f946f5726ce77538b129e8837e7e099d182e
Andrew Ward <award@award-Peppy.(none)>
2014-08-16 12:15:08 -0400

    Adding functions to get GLSB and LoadBalance members

--- app.py
+++ app.py
@@ -1,26 +1,40 @@
-from flask import Flask, flash, redirect, render_template
+from flask import Flask, redirect, render_template
 import requests
 import yaml
 
-config_yaml    = yaml.load(file("/Users/award/domains.yaml"))
+config_yaml = yaml.load(file("traffic-cop.yaml"))
 
 app = Flask(__name__)
 session_info = {
-            'customer_name': '',
-            'user_name'       : '',
-            'password'         : '',
+    'customer_name': config_yaml['user']['org'],
+    'user_name': config_yaml['user']['name'],
+    'password': config_yaml['user']['password'],
 }
 headers = {'content-type': 'application/json'}
 host = 'https://api.dynect.net/REST'
 
+
 def get_session():
-    return requests.post(host + '/Session/', params=session_info, headers=headers).json()['data']['token']
+    return requests.post(host + '/Session/', params=session_info,
+                         headers=headers).json()['data']['token']
+
 
 def get_load_balancers(domain):
-    formatted = []
-    for domain in requests.get( host + '/GSLB/' + domain, headers=headers).json()['data']:
-        formatted.append(domain.split('/')[4])
+    formatted = {}
+    for balancer in requests.get(host + '/GSLB/' + domain,
+                                 headers=headers).json()['data']:
+        formatted[domain] = balancer.split('/')[4].split('.')[:-2]
     return formatted
+
+
+def get_load_members(zone, fqdn):
+    lookup = host + '/LoadBalancePoolEntry/' + zone + '/' + fqdn
+    return requests.get(lookup, headers=headers).json()['data']
+
+
+def get_gslb_members(zone, fqdn):
+    lookup = host + '/GSLBRegionPoolEntry/' + zone + '/' + fqdn + '/global'
+    return requests.get(lookup, headers=headers).json()['data']
 
 
 @app.route('/login')
@@ -28,15 +42,17 @@ def login():
     headers['Auth-Token'] = get_session()
     return redirect('trafficcop')
 
+
 @app.route('/logout')
 def logout():
     requests.delete(host + '/Session/', headers=headers)
     headers['Auth-Token'] = ''
-    return  'Logged out'
+    return 'Logged out'
+
 
 @app.route('/trafficcop')
 def trafficcop():
-    balancers= []
+    balancers = []
     for domain in config_yaml['domains']:
         balancers.extend(get_load_balancers(domain))
     return render_template('trafficcop.html', balancers=balancers)
